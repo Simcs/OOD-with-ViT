98c552dfc10ccba6e090fa8f96dab3e9486f49d5
minho.sim <smh4477@naver.com>
2022-06-17 13:07:54 +0900

    return attnetion weights for visualization

--- models/vit.py
+++ models/vit.py
@@ -1,4 +1,4 @@
-# https://github.com/lucidrains/vit-pytorch/blob/main/vit_pytorch/vit.py
+from typing import Tuple, Union
 
 import torch
 from torch import nn
@@ -10,18 +10,19 @@ from einops.layers.torch import Rearrange
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
 
-# classes
 
 class PreNorm(nn.Module):
-    def __init__(self, dim, fn):
+    def __init__(self, dim: int, fn: nn.Module):
         super().__init__()
         self.norm = nn.LayerNorm(dim)
         self.fn = fn
+        
     def forward(self, x, **kwargs):
         return self.fn(self.norm(x), **kwargs)
 
+
 class FeedForward(nn.Module):
-    def __init__(self, dim, hidden_dim, dropout = 0.):
+    def __init__(self, dim: int, hidden_dim: int, dropout: float = 0.):
         super().__init__()
         self.net = nn.Sequential(
             nn.Linear(dim, hidden_dim),
@@ -30,25 +31,34 @@ class FeedForward(nn.Module):
             nn.Linear(hidden_dim, dim),
             nn.Dropout(dropout)
         )
+        
     def forward(self, x):
         return self.net(x)
 
+
 class Attention(nn.Module):
-    def __init__(self, dim, heads=8, dim_head=64, dropout=0.):
+    def __init__(self, 
+                 dim: int, 
+                 heads: int = 8, 
+                 dim_head: int = 64, 
+                 dropout: float = 0.,
+                 visualize: bool = False):
         super().__init__()
-        inner_dim = dim_head *  heads
+        inner_dim = dim_head * heads
         project_out = not (heads == 1 and dim_head == dim)
 
         self.heads = heads
         self.scale = dim_head ** -0.5
 
         self.attend = nn.Softmax(dim = -1)
-        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias=False)
 
         self.to_out = nn.Sequential(
             nn.Linear(inner_dim, dim),
             nn.Dropout(dropout)
         ) if project_out else nn.Identity()
+        
+        self.visualize = visualize
 
     def forward(self, x):
         qkv = self.to_qkv(x).chunk(3, dim=-1)
@@ -57,28 +67,63 @@ class Attention(nn.Module):
         dots = torch.matmul(q, k.transpose(-1, -2)) * self.scale
 
         attn = self.attend(dots)
+        attn_ = attn if self.visualize else None
 
         out = torch.matmul(attn, v)
         out = rearrange(out, 'b h n d -> b n (h d)')
-        return self.to_out(out)
+        out = self.to_out(out)
+        
+        return out, attn_
+
 
 class Transformer(nn.Module):
-    def __init__(self, dim, depth, heads, dim_head, mlp_dim, dropout = 0.):
+    def __init__(self, 
+                 dim: int, 
+                 depth: int, 
+                 heads: int, 
+                 dim_head: int, 
+                 mlp_dim: int, 
+                 dropout: float = 0.,
+                 visualize: bool = False):
         super().__init__()
         self.layers = nn.ModuleList([])
         for _ in range(depth):
             self.layers.append(nn.ModuleList([
-                PreNorm(dim, Attention(dim, heads = heads, dim_head = dim_head, dropout = dropout)),
-                PreNorm(dim, FeedForward(dim, mlp_dim, dropout = dropout))
+                PreNorm(dim, Attention(dim, 
+                                       heads=heads, 
+                                       dim_head=dim_head, 
+                                       dropout=dropout, 
+                                       visualize=visualize)),
+                PreNorm(dim, FeedForward(dim, mlp_dim, dropout=dropout))
             ]))
+            
     def forward(self, x):
+        attn_weights = []
         for attn, ff in self.layers:
-            x = attn(x) + x
+            h = x
+            x, weights = attn(x)
+            x = x + h
             x = ff(x) + x
-        return x
+            attn_weights.append(weights)
+        return x, attn_weights
+
 
 class ViT(nn.Module):
-    def __init__(self, *, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool='cls', channels=3, dim_head=64, dropout=0., emb_dropout=0.):
+    def __init__(self, 
+                 *, 
+                 image_size: Union[int, Tuple], 
+                 patch_size: Union[int, Tuple],
+                 num_classes: int,
+                 dim: int,
+                 depth: int,
+                 heads: int,
+                 mlp_dim: int, 
+                 pool: str = 'cls', 
+                 channels: int = 3, 
+                 dim_head: int = 64, 
+                 dropout: float = 0., 
+                 emb_dropout: float = 0.,
+                 visualize: bool = False):
         super().__init__()
         image_height, image_width = pair(image_size)
         patch_height, patch_width = pair(patch_size)
@@ -98,7 +143,7 @@ class ViT(nn.Module):
         self.cls_token = nn.Parameter(torch.randn(1, 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout)
+        self.transformer = Transformer(dim, depth, heads, dim_head, mlp_dim, dropout, visualize)
 
         self.pool = pool
         self.to_latent = nn.Identity()
@@ -117,9 +162,11 @@ class ViT(nn.Module):
         x += self.pos_embedding[:, :(n + 1)]
         x = self.dropout(x)
 
-        x = self.transformer(x)
+        x, attn_weights = self.transformer(x)
 
         x = x.mean(dim = 1) if self.pool == 'mean' else x[:, 0]
 
         x = self.to_latent(x)
-        return self.mlp_head(x)
+        out = self.mlp_head(x)
+        
+        return out, attn_weights
