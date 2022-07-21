14086ab5c07a3c877eaeaea639e97164aa47270e
minho.sim <smh4477@naver.com>
2022-07-21 16:41:15 +0900

    code refactoring

--- train_vit_ood_cifar10.py
+++ train_vit_ood_cifar10.py
@@ -9,40 +9,63 @@ from ml_collections import config_dict
 
 import torch
 
-from trainer import ViT_OOD_CIFAR10_Trainer
+from ood_with_vit.trainer import (
+    ViT_OOD_CIFAR10_Trainer, 
+    ViT_Finetune_OOD_CIFAR10_Trainer,
+)
 
+def create_trainer(config):
+    if config.model.pretrained:
+        trainer = ViT_Finetune_OOD_CIFAR10_Trainer(config)
+    else:
+        trainer = ViT_OOD_CIFAR10_Trainer(config)
+    return trainer
 
-def main(config):
-    # create log directories
-    log_root = Path('./logs')
-    checkpoint_path = log_root / 'checkpoints'
-    checkpoint_path.mkdir(parents=True, exist_ok=True)
+def main(config, args):
+    
+    trainer = create_trainer(config)
     
     # frequently used variables
     model_name = config.model.name
     base_lr = config.optimizer.base_lr
     batch_size = config.train.batch_size
-    n_indist_class = len(config.dataset.in_distribution_class_indices)
     patch_size = config.model.patch_size
+    log_epoch = config.train.log_epoch
+    summary = config.summary
+    
+    # create log directories
+    log_root = Path('./logs') / model_name
+    checkpoint_path = log_root / 'checkpoints'
+    checkpoint_path.mkdir(parents=True, exist_ok=True)
     
     # init wandb
-    name = f"{model_name}_OC{n_indist_class}_lr{base_lr}_bs{batch_size}"
+    name = f"{summary}_lr{base_lr}_bs{batch_size}"
     wandb.init(
-        project=f"ViT-OC{n_indist_class}-CIFAR10",
+        project=f"OOD-with-ViT",
         name=name,
         config=config,
     )
-    
-    best_acc = 0  # best test accuracy
-    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-    
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
     if config.train.scheduler == 'cosine':
         wandb.config.scheduler = "cosine"
     else:
         wandb.config.scheduler = "ReduceLROnPlateau"
-
+    
+    
+    best_acc = 0  # best test accuracy
+    start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+    
+    if args.resume:
+        # load checkpoint
+        print('resuming from checkpoint...')
+        assert os.path.isdir(checkpoint_path), 'Error: no checkpoint directory found!'
+        
+        checkpoint = torch.load(checkpoint_path / args.checkpoint)
+        start_epoch = checkpoint['epoch']
+        best_acc = checkpoint['best_acc']
+        trainer.model.load_state_dict(checkpoint['model_state_dict'])
+        trainer.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        trainer.scaler.load_state_dict(checkpoint['scaler_state_dict'])
+    
     wandb.watch(trainer.model)
     for epoch in range(start_epoch, config.train.n_epochs):
         print(f'\nEpoch: {epoch}')
@@ -51,9 +74,8 @@ def main(config):
         val_loss, val_acc = trainer.test()
         trainer.step_scheduler(val_loss)
         
-        # save checkpoint if best accuracy achieved.
-        if val_acc > best_acc:
-            print('Save checkpoint...')
+        # save checkpoint per every log epoch.
+        if epoch % log_epoch == 0:
             state = {
                 'epoch': epoch,
                 'best_acc': best_acc,
@@ -61,17 +83,29 @@ def main(config):
                 'optimizer_state_dict': trainer.optimizer.state_dict(),
                 'scaler_state_dict': trainer.scaler.state_dict(),
             }
-            torch.save(state, checkpoint_path / f'{model_name}-patch{patch_size}-ckpt.t7')
+            torch.save(state, checkpoint_path / f'{summary}_{epoch}.pt')
+            
+        # save checkpoint if best accuracy achieved.
+        if val_acc > best_acc:
+            print('Save checkpoint...')
             best_acc = val_acc
+            state = {
+                'epoch': epoch,
+                'best_acc': best_acc,
+                'model_state_dict': trainer.model.state_dict(),
+                'optimizer_state_dict': trainer.optimizer.state_dict(),
+                'scaler_state_dict': trainer.scaler.state_dict(),
+            }
+            torch.save(state, checkpoint_path / f'{summary}_best.pt')
         
         # log training info
         content = time.ctime() \
             + f' Epoch {epoch}, lr: {trainer.optimizer.param_groups[0]["lr"]:.7f}' \
             + f' val loss: {val_loss:.5f}, acc: {(val_acc):.5f}, time: {time.time() - start:.3f}'
-        print(content)
-        log_file = log_root / f'log_{model_name}_patch{patch_size}.txt'
+        log_file = log_root / f'{summary}.txt'
         with log_file.open(mode='a') as f:
             f.write(content + "\n")
+        print(content)
         
         # log to wandb
         wandb.log({
@@ -84,7 +118,7 @@ def main(config):
         })
 
     # writeout wandb
-    wandb.save(f"wandb_{model_name}.h5")
+    wandb.save(f"wandb_{summary}.h5")
     
     
 if __name__ == "__main__":
@@ -98,23 +132,7 @@ if __name__ == "__main__":
     with open(args.config) as config_file:
         config = yaml.safe_load(config_file) 
         config = config_dict.ConfigDict(config)
-    
-    trainer = ViT_OOD_CIFAR10_Trainer(config)
-        
-    if args.resume:
-        # Load checkpoint.
-        print('==> Resuming from checkpoint..')
-        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-        model_name = config.model.name
-        patch_size = config.model.patch_size
-        if model_name == 'vit':
-            checkpoint = torch.load(f'./checkpoint/{model_name}-patch{patch_size}-ckpt.t7')
-            start_epoch = checkpoint['epoch']
-            best_acc = checkpoint['best_acc']
-            trainer.model.load_state_dict(checkpoint['model_state_dict'])
-            trainer.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            trainer.scaler.load_state_dict(checkpoint['scaler_state_dict'])
-        
-    main(config)
+          
+    main(config, args)
 
 
