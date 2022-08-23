0c13e8a4996cfebebdfb91aba7c49626375a3d88
Simcs <smh4477@naver.com>
2022-08-24 00:24:54 +0900

    add hook/remove_hooks for managing forward hooks

--- ood_with_vit/visualizer/feature_extractor.py
+++ ood_with_vit/visualizer/feature_extractor.py
@@ -5,13 +5,23 @@ class FeatureExtractor(nn.Module):
     def __init__(self, model: nn.Module, layer_name: str):
         super().__init__()
         self.model = model
+        self.layer_name = layer_name
         self.features = []
-        for name, module in self.model.named_modules():
-            if layer_name in name:
-                module.register_forward_hook(self.get_features)
+        self.handles = []
 
     def get_features(self, module, input, output):
         self.features.append(output.detach().cpu())
+
+    def hook(self):
+        for name, module in self.model.named_modules():
+            if self.layer_name in name:
+                handle = module.register_forward_hook(self.get_features)
+                self.handles.append(handle)
+
+    def remove_hooks(self):
+        for handle in self.handles:
+            handle.remove()
+        self.handles = []
         
     def __call__(self, input):
         self.features = []
